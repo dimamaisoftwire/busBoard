b2455bb6916d09ccf53c26f87edefd6984f14fbf
Alex Welton <Alex.Welton@softwire.com>
2025-07-10 17:01:53 +0100

    Navigation Icon

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -6,7 +6,16 @@ import React from "react";
 const Navigation = (): React.ReactElement => {
     return <Navbar expand="lg" className="bg-body-tertiary" style={{height:"10%"}}>
         <Container>
-            <Navbar.Brand href="home">Bus Board</Navbar.Brand>
+            <Navbar.Brand href="home">
+                <img
+                    alt=""
+                    src="/icon.png"
+                    width="30"
+                    height="30"
+                    className="d-inline-block align-top"
+                />{' '}
+                BusBoard
+            </Navbar.Brand>
             <Navbar.Toggle aria-controls="basic-navbar-nav" />
             <Navbar.Collapse id="basic-navbar-nav">
                 <Nav className="me-auto">
