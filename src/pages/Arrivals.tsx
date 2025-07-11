ebe45255b35b72a9c6892497b9fa83b4f4e22285
Alex Welton <Alex.Welton@softwire.com>
2025-07-11 09:09:37 +0100

    Fixes

--- src/pages/Arrivals.tsx
+++ src/pages/Arrivals.tsx
@@ -10,6 +10,7 @@ const SECOND = 1000;
 const TABLE_REFRESH_SECONDS = 10;
 
 async function getBuses(stopPoint: StopPoint): Promise<BusDetails[] | undefined> {
+  if (!stopPoint) return undefined;
   return await getStopPointNextNArrivals(stopPoint,5);
 }
 
@@ -83,16 +84,23 @@ function Arrivals(): React.ReactElement {
         .then(
             (stopPoints) => {
               if (stopPoints !== undefined) {
-                getBuses(stopPoints[0])
-                    .then((busData) => {
-                      setFirstTableTitle(stopPoints[0].commonName);
-                      setFirstTableData(busData);
-                    });
-                getBuses(stopPoints[1])
-                    .then((busData) => {
-                      setSecondTableTitle(stopPoints[1].commonName);
-                      setSecondTableData(busData);
-                    });
+                if (stopPoints[0] !== undefined ) {
+                  getBuses(stopPoints[0])
+                      .then((busData) => {
+                        setFirstTableTitle(stopPoints[0].commonName);
+                        setFirstTableData(busData);
+                      });
+                }
+
+                if (stopPoints[1] !== undefined ) {
+                  getBuses(stopPoints[1])
+                      .then((busData) => {
+                        setSecondTableTitle(stopPoints[1].commonName);
+                        setSecondTableData(busData);
+                      });
+                }
+
+
               }
 
             });
